acc1f149a615ba2cfff878978d7eb88c72f254a4
Thibaud Colas <thibaudcolas@gmail.com>
2015-01-18 21:52:20 +1300

    Switch to ROT-18 to handle numbers

--- email-scramble.js
+++ email-scramble.js
@@ -10,25 +10,22 @@
 }(this, function (root) {
   'use strict';
 
-  // https://github.com/mathiasbynens/rot
-  var rot = function rot(str, n) {
-    /* jshint ignore:start */
+  // Largely taken from https://github.com/mathiasbynens/rot.
+  var rot = function rot(str, charRot, numRot) {
+    var numbers = '0123456789';
     var lowercase = 'abcdefghijklmnopqrstuvwxyz';
     var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var regexNumber = /[0-9]/;
     var regexLowercase = /[a-z]/;
     var regexUppercase = /[A-Z]/;
 
-    if (n == null) {
-      // use ROT-13 by default
-      n = 13;
-    }
-    n = Number(n);
     str = String(str);
-    if (n == 0) {
-      return str;
+
+    if (charRot < 0) {
+      charRot += 26;
     }
-    if (n < 0) { // decode instead of encode
-      n += 26;
+    if (numRot < 0) {
+      numRot += 10;
     }
     var length = str.length; // note: no need to account for astral symbols
     var index = -1;
@@ -38,37 +35,32 @@
     var shiftedPosition;
     while (++index < length) {
       character = str.charAt(index);
-      if (regexLowercase.test(character)) {
+      if (regexNumber.test(character)) {
+        currentPosition = numbers.indexOf(character);
+        shiftedPosition = (currentPosition + numRot) % 10;
+        result += numbers.charAt(shiftedPosition);
+      } else if (regexLowercase.test(character)) {
         currentPosition = lowercase.indexOf(character);
-        shiftedPosition = (currentPosition + n) % 26;
+        shiftedPosition = (currentPosition + charRot) % 26;
         result += lowercase.charAt(shiftedPosition);
       } else if (regexUppercase.test(character)) {
         currentPosition = uppercase.indexOf(character);
-        shiftedPosition = (currentPosition + n) % 26;
+        shiftedPosition = (currentPosition + charRot) % 26;
         result += uppercase.charAt(shiftedPosition);
       } else {
         result += character;
       }
     }
     return result;
-    /* jshint ignore:end */
   };
 
-  var encoder = function encoder(n) {
-    return function encode(str) {
-      return rot(str, n);
-    };
-  };
-
-  var decoder = function decoder(n) {
-    return function decode(str) {
-      return rot(str, -n);
-    };
+  var rot18 = function rot18(str) {
+    return rot(str, 13, 5);
   };
 
   return {
-    _rot: rot,
-    encoder: encoder,
-    decoder: decoder
+    rot: rot,
+    encode: rot18,
+    decode: rot18
   };
 }));
--- test/email-scramble-tests.js
+++ test/email-scramble-tests.js
@@ -3,188 +3,164 @@
 var expect = require('chai').expect;
 var emailScramble = require('../email-scramble');
 
+var testString = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+
+var rot = emailScramble.rot;
+var encode = emailScramble.encode;
+var decode = emailScramble.decode;
+
 describe('email-scramble', function() {
-  var testString = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~';
-
-  var rot = emailScramble._rot;
-  var encoder = emailScramble.encoder;
-  var decoder = emailScramble.decoder;
-
   it('is available as a Node module', function() {
     expect(emailScramble).to.exist();
     expect(emailScramble).to.be.an('object');
   });
 
   it('exposes a small API', function() {
-    expect(emailScramble).to.respondTo('_rot');
-    expect(emailScramble).to.respondTo('encoder');
-    expect(emailScramble).to.respondTo('decoder');
+    expect(emailScramble).to.respondTo('rot');
+    expect(emailScramble).to.respondTo('encode');
+    expect(emailScramble).to.respondTo('decode');
   });
 
   describe('ROT implementation', function() {
     it('ROT-0', function() {
-      expect(rot(testString, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~');
+      expect(rot(testString, 0, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~');
     });
 
     it('ROT-1', function() {
-      expect(rot(testString, 1)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@BCDEFGHIJKLMNOPQRSTUVWXYZA[]^_`bcdefghijklmnopqrstuvwxyza{|}~');
+      expect(rot(testString, 1, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@BCDEFGHIJKLMNOPQRSTUVWXYZA[]^_`bcdefghijklmnopqrstuvwxyza{|}~');
     });
 
     it('ROT-2', function() {
-      expect(rot(testString, 2)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@CDEFGHIJKLMNOPQRSTUVWXYZAB[]^_`cdefghijklmnopqrstuvwxyzab{|}~');
+      expect(rot(testString, 2, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@CDEFGHIJKLMNOPQRSTUVWXYZAB[]^_`cdefghijklmnopqrstuvwxyzab{|}~');
     });
 
     it('ROT-3', function() {
-      expect(rot(testString, 3)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@DEFGHIJKLMNOPQRSTUVWXYZABC[]^_`defghijklmnopqrstuvwxyzabc{|}~');
+      expect(rot(testString, 3, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@DEFGHIJKLMNOPQRSTUVWXYZABC[]^_`defghijklmnopqrstuvwxyzabc{|}~');
     });
 
     it('ROT-4', function() {
-      expect(rot(testString, 4)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@EFGHIJKLMNOPQRSTUVWXYZABCD[]^_`efghijklmnopqrstuvwxyzabcd{|}~');
-    });
-
-    it('ROT-4', function() {
-      expect(rot(testString, 4)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@EFGHIJKLMNOPQRSTUVWXYZABCD[]^_`efghijklmnopqrstuvwxyzabcd{|}~');
+      expect(rot(testString, 4, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@EFGHIJKLMNOPQRSTUVWXYZABCD[]^_`efghijklmnopqrstuvwxyzabcd{|}~');
     });
 
     it('ROT-5', function() {
-      expect(rot(testString, 5)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@FGHIJKLMNOPQRSTUVWXYZABCDE[]^_`fghijklmnopqrstuvwxyzabcde{|}~');
-    });
-
-    it('ROT-5: decode', function() {
-      expect(rot(' !"#$%&\'()*+,-./0123456789:;<=>?@FGHIJKLMNOPQRSTUVWXYZABCDE[]^_`fghijklmnopqrstuvwxyzabcde{|}~', -5)).to.equal(testString);
-    });
-
-    it('ROT-5: decode', function() {
-      expect(rot(' !"#$%&\'()*+,-./0123456789:;<=>?@FGHIJKLMNOPQRSTUVWXYZABCDE[]^_`fghijklmnopqrstuvwxyzabcde{|}~', 26 - 5)).to.equal(testString);
+      expect(rot(testString, 5, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@FGHIJKLMNOPQRSTUVWXYZABCDE[]^_`fghijklmnopqrstuvwxyzabcde{|}~');
     });
 
     it('ROT-6', function() {
-      expect(rot(testString, 6)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@GHIJKLMNOPQRSTUVWXYZABCDEF[]^_`ghijklmnopqrstuvwxyzabcdef{|}~');
+      expect(rot(testString, 6, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@GHIJKLMNOPQRSTUVWXYZABCDEF[]^_`ghijklmnopqrstuvwxyzabcdef{|}~');
     });
 
     it('ROT-7', function() {
-      expect(rot(testString, 7)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@HIJKLMNOPQRSTUVWXYZABCDEFG[]^_`hijklmnopqrstuvwxyzabcdefg{|}~');
+      expect(rot(testString, 7, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@HIJKLMNOPQRSTUVWXYZABCDEFG[]^_`hijklmnopqrstuvwxyzabcdefg{|}~');
     });
 
     it('ROT-8', function() {
-      expect(rot(testString, 8)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@IJKLMNOPQRSTUVWXYZABCDEFGH[]^_`ijklmnopqrstuvwxyzabcdefgh{|}~');
+      expect(rot(testString, 8, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@IJKLMNOPQRSTUVWXYZABCDEFGH[]^_`ijklmnopqrstuvwxyzabcdefgh{|}~');
     });
 
     it('ROT-9', function() {
-      expect(rot(testString, 9)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@JKLMNOPQRSTUVWXYZABCDEFGHI[]^_`jklmnopqrstuvwxyzabcdefghi{|}~');
+      expect(rot(testString, 9, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@JKLMNOPQRSTUVWXYZABCDEFGHI[]^_`jklmnopqrstuvwxyzabcdefghi{|}~');
     });
 
     it('ROT-10', function() {
-      expect(rot(testString, 10)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@KLMNOPQRSTUVWXYZABCDEFGHIJ[]^_`klmnopqrstuvwxyzabcdefghij{|}~');
+      expect(rot(testString, 10, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@KLMNOPQRSTUVWXYZABCDEFGHIJ[]^_`klmnopqrstuvwxyzabcdefghij{|}~');
     });
 
     it('ROT-11', function() {
-      expect(rot(testString, 11)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@LMNOPQRSTUVWXYZABCDEFGHIJK[]^_`lmnopqrstuvwxyzabcdefghijk{|}~');
+      expect(rot(testString, 11, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@LMNOPQRSTUVWXYZABCDEFGHIJK[]^_`lmnopqrstuvwxyzabcdefghijk{|}~');
     });
 
     it('ROT-12', function() {
-      expect(rot(testString, 12)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@MNOPQRSTUVWXYZABCDEFGHIJKL[]^_`mnopqrstuvwxyzabcdefghijkl{|}~');
-    });
-
-    it('ROT-13 (default)', function() {
-      expect(rot(testString)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@NOPQRSTUVWXYZABCDEFGHIJKLM[]^_`nopqrstuvwxyzabcdefghijklm{|}~');
+      expect(rot(testString, 12, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@MNOPQRSTUVWXYZABCDEFGHIJKL[]^_`mnopqrstuvwxyzabcdefghijkl{|}~');
     });
 
     it('ROT-13', function() {
-      expect(rot(testString, 13)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@NOPQRSTUVWXYZABCDEFGHIJKLM[]^_`nopqrstuvwxyzabcdefghijklm{|}~');
+      expect(rot(testString, 13, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@NOPQRSTUVWXYZABCDEFGHIJKLM[]^_`nopqrstuvwxyzabcdefghijklm{|}~');
     });
 
     it('ROT-14', function() {
-      expect(rot(testString, 14)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@OPQRSTUVWXYZABCDEFGHIJKLMN[]^_`opqrstuvwxyzabcdefghijklmn{|}~');
+      expect(rot(testString, 14, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@OPQRSTUVWXYZABCDEFGHIJKLMN[]^_`opqrstuvwxyzabcdefghijklmn{|}~');
     });
 
     it('ROT-15', function() {
-      expect(rot(testString, 15)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@PQRSTUVWXYZABCDEFGHIJKLMNO[]^_`pqrstuvwxyzabcdefghijklmno{|}~');
+      expect(rot(testString, 15, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@PQRSTUVWXYZABCDEFGHIJKLMNO[]^_`pqrstuvwxyzabcdefghijklmno{|}~');
     });
 
     it('ROT-16', function() {
-      expect(rot(testString, 16)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@QRSTUVWXYZABCDEFGHIJKLMNOP[]^_`qrstuvwxyzabcdefghijklmnop{|}~');
+      expect(rot(testString, 16, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@QRSTUVWXYZABCDEFGHIJKLMNOP[]^_`qrstuvwxyzabcdefghijklmnop{|}~');
     });
 
     it('ROT-17', function() {
-      expect(rot(testString, 17)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@RSTUVWXYZABCDEFGHIJKLMNOPQ[]^_`rstuvwxyzabcdefghijklmnopq{|}~');
+      expect(rot(testString, 17, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@RSTUVWXYZABCDEFGHIJKLMNOPQ[]^_`rstuvwxyzabcdefghijklmnopq{|}~');
     });
 
     it('ROT-18', function() {
-      expect(rot(testString, 18)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@STUVWXYZABCDEFGHIJKLMNOPQR[]^_`stuvwxyzabcdefghijklmnopqr{|}~');
+      expect(rot(testString, 18, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@STUVWXYZABCDEFGHIJKLMNOPQR[]^_`stuvwxyzabcdefghijklmnopqr{|}~');
     });
 
     it('ROT-19', function() {
-      expect(rot(testString, 19)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@TUVWXYZABCDEFGHIJKLMNOPQRS[]^_`tuvwxyzabcdefghijklmnopqrs{|}~');
+      expect(rot(testString, 19, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@TUVWXYZABCDEFGHIJKLMNOPQRS[]^_`tuvwxyzabcdefghijklmnopqrs{|}~');
     });
 
     it('ROT-20', function() {
-      expect(rot(testString, 20)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@UVWXYZABCDEFGHIJKLMNOPQRST[]^_`uvwxyzabcdefghijklmnopqrst{|}~');
+      expect(rot(testString, 20, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@UVWXYZABCDEFGHIJKLMNOPQRST[]^_`uvwxyzabcdefghijklmnopqrst{|}~');
     });
 
     it('ROT-21', function() {
-      expect(rot(testString, 21)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@VWXYZABCDEFGHIJKLMNOPQRSTU[]^_`vwxyzabcdefghijklmnopqrstu{|}~');
+      expect(rot(testString, 21, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@VWXYZABCDEFGHIJKLMNOPQRSTU[]^_`vwxyzabcdefghijklmnopqrstu{|}~');
     });
 
     it('ROT-22', function() {
-      expect(rot(testString, 22)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@WXYZABCDEFGHIJKLMNOPQRSTUV[]^_`wxyzabcdefghijklmnopqrstuv{|}~');
+      expect(rot(testString, 22, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@WXYZABCDEFGHIJKLMNOPQRSTUV[]^_`wxyzabcdefghijklmnopqrstuv{|}~');
     });
 
     it('ROT-23', function() {
-      expect(rot(testString, 23)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@XYZABCDEFGHIJKLMNOPQRSTUVW[]^_`xyzabcdefghijklmnopqrstuvw{|}~');
+      expect(rot(testString, 23, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@XYZABCDEFGHIJKLMNOPQRSTUVW[]^_`xyzabcdefghijklmnopqrstuvw{|}~');
     });
 
     it('ROT-24', function() {
-      expect(rot(testString, 24)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@YZABCDEFGHIJKLMNOPQRSTUVWX[]^_`yzabcdefghijklmnopqrstuvwx{|}~');
+      expect(rot(testString, 24, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@YZABCDEFGHIJKLMNOPQRSTUVWX[]^_`yzabcdefghijklmnopqrstuvwx{|}~');
     });
 
     it('ROT-25', function() {
-      expect(rot(testString, 25)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@ZABCDEFGHIJKLMNOPQRSTUVWXY[]^_`zabcdefghijklmnopqrstuvwxy{|}~');
+      expect(rot(testString, 25, 0)).to.equal(' !"#$%&\'()*+,-./0123456789:;<=>?@ZABCDEFGHIJKLMNOPQRSTUVWXY[]^_`zabcdefghijklmnopqrstuvwxy{|}~');
     });
 
     it('ROT-26 == ROT-0', function() {
-      expect(rot(testString, 26)).to.equal(rot(testString, 0));
+      expect(rot(testString, 26, 0)).to.equal(rot(testString, 0, 0));
     });
   });
 
   describe('encode & decode', function() {
     it('encodes by using the ROT implementation', function() {
-      for (var i = 0; i < 27; i++) {
-        expect(encoder(i)(testString)).to.equal(rot(testString, i));
-      }
+      expect(encode(testString)).to.equal(rot(testString, 13, 5));
     });
 
     it('decodes by using the ROT implementation', function() {
-      for (var i = 0; i < 27; i++) {
-        expect(decoder(i)(testString)).to.equal(rot(testString, -i));
-      }
+      expect(decode(testString)).to.equal(rot(testString, 13, 5));
     });
 
     it('can be chained together', function() {
-      for (var i = 0; i < 27; i++) {
-        expect(decoder(i)(encoder(i)(testString))).to.equal(testString);
-        expect(encoder(i)(decoder(i)(testString))).to.equal(testString);
-      }
+      expect(decode(encode(testString))).to.equal(testString);
+      expect(encode(decode(testString))).to.equal(testString);
     });
   });
 
   describe('emails', function() {
     it('can be encoded with encode', function() {
-      var encode = encoder(5);
-      expect(encode('mail@example.com')).to.equal(('rfnq@jcfruqj.htr'));
-      expect(encode('testmail@example.com')).to.equal(('yjxyrfnq@jcfruqj.htr'));
-      expect(encode('mail@testexample.com')).to.equal(('rfnq@yjxyjcfruqj.htr'));
-      expect(encode('mail@example.test')).to.equal(('rfnq@jcfruqj.yjxy'));
-      expect(encode('mail+ref@example.com')).to.equal(('rfnq+wjk@jcfruqj.htr'));
+      expect(encode('mail@example.com')).to.equal(('znvy@rknzcyr.pbz'));
+      expect(encode('testmail@example.com')).to.equal(('grfgznvy@rknzcyr.pbz'));
+      expect(encode('mail@testexample.com')).to.equal(('znvy@grfgrknzcyr.pbz'));
+      expect(encode('mail@example.test')).to.equal(('znvy@rknzcyr.grfg'));
+      expect(encode('mail+ref@example.com')).to.equal(('znvy+ers@rknzcyr.pbz'));
     });
 
     it('can be decoded with decode', function() {
-      var decode = decoder(5);
-      expect(decode('rfnq@jcfruqj.htr')).to.equal(('mail@example.com'));
-      expect(decode('yjxyrfnq@jcfruqj.htr')).to.equal(('testmail@example.com'));
-      expect(decode('rfnq@yjxyjcfruqj.htr')).to.equal(('mail@testexample.com'));
-      expect(decode('rfnq@jcfruqj.yjxy')).to.equal(('mail@example.test'));
-      expect(decode('rfnq+wjk@jcfruqj.htr')).to.equal(('mail+ref@example.com'));
+      expect(decode('znvy@rknzcyr.pbz')).to.equal(('mail@example.com'));
+      expect(decode('grfgznvy@rknzcyr.pbz')).to.equal(('testmail@example.com'));
+      expect(decode('znvy@grfgrknzcyr.pbz')).to.equal(('mail@testexample.com'));
+      expect(decode('znvy@rknzcyr.grfg')).to.equal(('mail@example.test'));
+      expect(decode('znvy+ers@rknzcyr.pbz')).to.equal(('mail+ref@example.com'));
     });
   });
 
